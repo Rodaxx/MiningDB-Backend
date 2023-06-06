733f3a4ff17a119919b8ceb00a9bc585a85e256b
Rodrigo Vega <rodaxx30@outlook.com>
2023-06-06 00:50:33 -0400

    Conexion a DB

--- config_mssql.js
+++ config_mssql.js
@@ -6,6 +6,9 @@ const sqlConfig = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   server: process.env.DB_HOST,
+  options: {
+    trustServerCertificate: true,
+  }
 }
 
 const poolPromise = new sql.ConnectionPool(sqlConfig)
--- utils/csv.js
+++ utils/csv.js
@@ -1,53 +1,53 @@
 const fs = require('fs');
 const { poolPromise , sql } = require('../config_mssql');
 
-
-const filename = 'excel/ejemplo.csv';
-const fileText = fs.readFileSync(filename, 'utf-8');
-
-const allLines = fileText.split(/\r\n|\r|\n/, -1);
-
-const headers = allLines[0];
-const fieldNames = headers.split(';');
-fieldNames[2] = 'camion';
-
-let objList = [];
-for (let i = 1; i < allLines.length; i++){
-    let obj = {};
-    const data = allLines[i].split(';');
-
-    for (let j = 0; j < fieldNames.length; j++){
-        const fieldName = fieldNames[j].toLowerCase();
-        obj[fieldName] = data[j];
+function leerCsv(filename){
+    const fileText = fs.readFileSync(filename, 'utf-8');
+    
+    const allLines = fileText.split(/\r\n|\r|\n/, -1);
+    
+    const headers = allLines[0];
+    const fieldNames = headers.split(';');
+    fieldNames[2] = 'camion';
+    
+    let objList = [];
+    for (let i = 1; i < allLines.length; i++){
+        let obj = {};
+        const data = allLines[i].split(';');
+        
+        for (let j = 0; j < fieldNames.length; j++){
+            const fieldName = fieldNames[j].toLowerCase();
+            obj[fieldName] = data[j];
+        }
+        objList.push(obj);
     }
-    objList.push(obj);
+    return objList;
 }
-/** 
-const jsonText = JSON.stringify(objList);
-console.log(JSON.parse(jsonText));
-*/
-
-for (let i = 0; i<objList.length; i++){
-    try{
-        const pool = await poolPromise;
-        const result = await pool.request()
-            .input("id_fecha",sql.Int,  objList[i].fecha.replace("-",""))
-            .input("id_pala",sql.SmallInt, objList[i].carguio)
-            .query("INSERT INTO Ciclo.ciclo (id_fecha, id_pala) VALUES (@id_fecha, @id_pala)")
+async function cargarDatos(){
+    try {
+        for (let i = 0; i<objList.length; i++){
+            const pool = await poolPromise;
+            const result = await pool.request()
+                .input("fecha",sql.Date,  objList[i].fecha)
+                .input("pala",sql.VarChar, objList[i].carguio)
+                .input("camion", sql.VarChar, objList[i].camion)
+                .input("flota", sql.VarChar, objList[i].flota)
+                .input("material", sql.VarChar, objList[i].material)
+                .input("origen", sql.VarChar, objList[i].origen)
+                .input("zona", sql.VarChar, objList[i].zona)
+                .input("destino", sql.VarChar, objList[i].destino)
+                .input("tonelaje", sql.Real, objList[i].tonelaje)
+                .input("ciclos", sql.Real, objList[i].ciclos)
+                .input("rajo", sql.VarChar, objList[i].rajo)
+                .query("INSERT INTO ciclo (fecha, pala, camion, flota, material, origen, zona, destino, tonelaje, ciclos, rajo) VALUES (@fecha, @pala, @camion, @flota, @material, @origen, @zona, @destino, @tonelaje, @ciclos, @rajo)");
+        }
     }
-    catch{
-
+    catch (err){
+        console.error(err);
     }
-    //console.log(objList[0]);
+    finally{
+        pool.close();
+    }
 }
 
-/**  
-try{
-    const pool = await poolPromise
-    const result = await pool.request()
-    .input()
-    .query()
-}catch (err){
-    console.log(err.message);
-}
-*/
+cargarDatos(leerCsv('excel/ejemplo.csv'));
