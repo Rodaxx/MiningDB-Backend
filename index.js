eade4905d4a5e3f183e2c7748a072a3f2314318b
Rodrigo Vega <rodaxx30@outlook.com>
2023-07-15 00:28:08 -0400

    Change default port and add cors

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const login_routes = require('./routes/login');
 const users_routes = require('./routes/users')
 
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 //Auto documentacion
 const swagger = require('./docs/swagger');
 swagger(app);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,10 +22,10 @@ app.use('/users', users_routes);
 
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.send('Backend MiningDB');
 });
 
 app.listen(port, () => {
-  console.log(`El servidor está escuchando en el puerto ${port}`);
+  console.log(`Servidor iniciado!`);
 });
 
