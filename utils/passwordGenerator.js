e05813ce47d2ba49fa253bf3c23bd04300c4fcd5
Rodrigo Vega <rodaxx30@outlook.com>
2023-07-15 04:03:05 -0400

    add password generator

---
+++ utils/passwordGenerator.js
@@ -0,0 +1,15 @@
+const generatePassword = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
+    }
+    return password;
+  }
+
+module.exports = {
+    generatePassword
+}
+
+  
