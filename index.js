c872c57d06176e7983be298bc9584ae4906bda62
Rodrigo Vega <rodaxx30@outlook.com>
2023-07-18 00:31:02 -0400

    add root routes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const login_routes = require('./routes/login');
 const users_routes = require('./routes/users')
+const root_routes = require('./routes/root')
 
 const app = express();
 const port = 80;
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/login', login_routes);
 app.use('/users', users_routes);
+app.use('/root', root_routes)
 
 app.listen(port, () => {
   console.log(`Servidor iniciado!`);
