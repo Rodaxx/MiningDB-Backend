bd88a4f1e736b5bc547620c438068cf81d5a0d13
Rodrigo Vega <rodaxx30@outlook.com>
2023-07-19 20:18:56 -0400

    add new routes

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const express = require('express');
 const cors = require('cors');
 
 const login_routes = require('./routes/login');
-const users_routes = require('./routes/users')
 const root_routes = require('./routes/root')
+const admin_routes = require('./routes/admin')
+const users_routes = require('./routes/users')
+const csv_route = require('./csv');
 
 const app = express();
 const port = 80;
+
+const firstRun = require('./createDatabase')
+app.get('/database', (req, res) => {
+  firstRun();
+  return res.sendStatus(200);
+})
 
 // FTP Server
 if (process.env.FTP_ENABLED == 'true'){
   const server = require('./ftp/ftp_service').server
   server.listen()
 }
-
 
 //Auto documentacion
 const swagger = require('./docs/swagger');
@@ -26,8 +33,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/login', login_routes);
+app.use('/root', root_routes);
+app.use('/admin', admin_routes);
 app.use('/users', users_routes);
-app.use('/root', root_routes)
+app.use('/csv', csv_route )
 
 app.listen(port, () => {
   console.log(`Servidor iniciado!`);
